064b14d26bf264adcdf262171b333cb139fc53d0
riot <quadcresent@gmail.com>
2026-09-17 11:23:29 +0000

    chore: remove one-off mutation scripts and redundant db migrators

--- check_db.js
+++
@@ -1,5 +0,0 @@
-const { neon } = require('@neondatabase/serverless');
-const fs = require('fs');
-const env = fs.readFileSync('.env.local', 'utf8').split('\n').find(l => l.startsWith('DATABASE_URL=')).split('=')[1].replace(/"/g, '');
-const sql = neon(env);
-sql.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`).then(console.log).catch(console.error);
